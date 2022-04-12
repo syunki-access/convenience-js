933037ff8ed8192cb324c47e310fa80b476e1b8e
Toshitaka Kataoka <kataoka@mynet.co.jp>
2022-04-12 18:37:34 +0900

    【react】makeStylesのサンプル実装

--- wiki-sample-react/src/access/index.js
+++ wiki-sample-react/src/access/index.js
@@ -1,9 +1,33 @@
+import pageContents from './page.json';
+import { 
+  makeStyles,
+} from '@material-ui/core/styles';
 
+const useStyles = makeStyles({
+  main: {
+    '& .Ssize':{
+      fontSize: "50%",
+    },
+    '& .Msize':{
+      fontSize: "100%",
+    },
+    '& .Lsize':{
+      fontSize: "200%",
+    },
+  }
+});
 
 const Access = (props) => {
-  return (<>
-    <h1>accessのページ</h1>
-  </>)
+  console.log('pageContents', pageContents);
+  const classes = useStyles();
+  return (<div className={classes.main}>
+    <h1>accessのページあいうえお</h1>
+    <div>
+      <p className='Ssize'>Ssizeの文字ですよ。</p><br />
+      <p className='Msize'>Msizeの文字ですよ。</p><br />
+      <p className='Lsize'>Lsizeの文字ですよ。</p><br />
+    </div>
+  </div>)
 };
 
 export default Access;
