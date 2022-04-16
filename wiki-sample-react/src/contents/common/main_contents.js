a327602821aaa91addf4282f3d53ade495b13f5e
syunki <syunki-access@k3.dion.ne.jp>
2022-04-16 11:54:17 +0900

    変数書き換え

--- wiki-sample-react/src/contents/common/main_contents.js
+++ wiki-sample-react/src/contents/common/main_contents.js
@@ -3,17 +3,16 @@ import React from "react";
 
 const MainContents = (props) => {
   const _list = props.mainList;
-  console.log('_list', _list);
   return (<>
-    {_list.map((val, index) => {
-      return (<React.Fragment key={index}>
-        <div class="main-box contents" id={val.titleId}>
-          <h2 class="title-text">{val.titleText}</h2>
-          <div class="main-text">
+    {_list.map((__val, __index) => { //reactの「map」は配列にしか使えない！！
+      return (<React.Fragment key={__index}>
+        <div className="main-box contents" id={__val.titleId}>
+          <h2 className="title-text">{__val.titleText}</h2>
+          <div className="main-text">
             コンテンツの中身です
           </div>
-          <div class="content-footer">
-            <span class="anker">▲ ページトップに戻る ▲</span>
+          <div className="content-footer">
+            <span className="anker">▲ ページトップに戻る ▲</span>
           </div>
         </div>
       </React.Fragment>)
