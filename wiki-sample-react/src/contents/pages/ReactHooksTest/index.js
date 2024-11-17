798e5957a50a4bf27c929029a2713fdde3a80b51
syunki <syunki-access@k3.dion.ne.jp>
2024-11-17 17:00:16 +0900

    【ReactHooksTest】useContextで親の操作をできるように修正

--- wiki-sample-react/src/contents/pages/ReactHooksTest/index.js
+++ wiki-sample-react/src/contents/pages/ReactHooksTest/index.js
@@ -1,43 +1,70 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import UseContextTest01 from "./useContextTest01";
 
 // useContextのテスト用データ
-const propsData = {
+let propsDataOrg = {
   name: 'あいうえお',
   data1: 'カキクケコ',
   data2: 'さしすせそ',
   count: 0,
+  handleClick: () => {
+    console.log('元handleClickです');
+  },
 }
 // createContextで定義しておく
-export const PropsDataContext = createContext(propsData);
+export const PropsDataContext = createContext(propsDataOrg);
 
 const ReactHooksTest = (props) => {
   //let count = 0;
   const [count, setCount] = useState(0);
 
-  const handleClick = () => {
-    let _count = count + 1;
+  const handleClick = (_args) => {
+    let _count = propsDataOrg.count;
+    if(_args == 'plus'){
+      _count = _count + 1;
+    }else if(_args == 'minus'){
+      _count = _count - 1;
+    };
     setCount(_count);
     console.log('count', _count);
     // useContext用データも更新
-    propsData.count = _count;
-    console.log('propsData.count', propsData.count);
+    propsDataOrg.count = _count;
+    console.log('propsData.count', propsDataOrg.count);
   };
+
   useEffect(() => {
     console.log('useEffect起動した');
-  }, [count]);
-  
+    propsDataOrg.handleClick(); //最初に実行しておく
+    //handleClickを持っていく
+    propsDataOrg.handleClick = handleClick;
+  }, []);
+
+  // useRefを定義しておくことでjqueryのセレクタみたくいろんな値を参照することができる
+  const inputText = useRef();
+  const handleRef = () => {
+    console.log('inputText', inputText);
+    console.log('inputText.current.value', inputText.current.value);
+  };
+
   return (<>
     <h1>ReactHooksのお勉強</h1>
+
     <hr/>
     <h2>useState・useEffect</h2>
-    <button onClick={handleClick}>＋</button>
+    <button onClick={() => handleClick('plus')}>＋</button>
+    <button onClick={() => handleClick('minus')}>−</button>
     <p>{count}</p>
+
     <hr/>
-    <PropsDataContext.Provider value={propsData}>
+    <PropsDataContext.Provider value={propsDataOrg}>
       <h2>useContext</h2>
       <UseContextTest01 />
     </PropsDataContext.Provider>
+
+    <hr/>
+    <h2>useRef</h2>
+    <input type="text" ref={inputText} />
+    <button onClick={handleRef}>UseRef</button>
   </>)
 };
 
--- wiki-sample-react/src/contents/pages/ReactHooksTest/useContextTest02.js
+++ wiki-sample-react/src/contents/pages/ReactHooksTest/useContextTest02.js
@@ -2,7 +2,15 @@ import { useContext } from "react";
 import { PropsDataContext } from ".";
 
 const useContextTest02 = () => {
-  const propsData = useContext(PropsDataContext)
+  //Context用データ
+  let propsData = useContext(PropsDataContext);
+  //操作関数
+  //呼び出されるコンポーネントでも親の値を更新できる
+  const tapButton = (_args) => {
+    propsData.handleClick(_args);
+    console.log('useContextTest02.propsData.count', propsData.count);
+  };
+
   return (<>
     <h3>useContextTest02</h3>
     <div>
@@ -11,6 +19,15 @@ const useContextTest02 = () => {
       <p>データ1: {propsData.data1}</p>
       <p>データ2: {propsData.data2}</p>
       <p>カウント: {propsData.count}</p>
+    </div>
+    <div>
+      カウント操作：
+      {
+        // jsx内で関数を呼び出して引数を持たせたいときは
+        // 即時関数 () => hoge('') を使う
+      }
+      <button onClick={() => tapButton('plus')}>＋</button>
+      <button onClick={() => tapButton('minus')}>−</button>
     </div>
   </>)
 };
